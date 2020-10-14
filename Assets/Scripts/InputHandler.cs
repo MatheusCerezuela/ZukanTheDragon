e0d9433aaab18d0d43591c4c36f5fe557de4e901
Ricardo <ricardolemedossantos@yahoo.com>
2020-10-14 16:32:09 -0300

    Atualizei o InputHandler

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -15,31 +15,14 @@ namespace DS
         public bool b_Input;
 
         public bool rollFlag;
-        public bool isInteracting;
         public bool sprintFlag;
         public float rollInputTimer;
 
         PlayerControls inputActions;
-        CameraHandler cameraHandler;
 
         Vector2 movementInput;
         Vector2 cameraInput;
 
-        private void Awake()
-        {
-            cameraHandler = CameraHandler.singleton;
-        }
-
-        private void FixedUpdate()
-        {
-            float delta = Time.fixedDeltaTime;
-
-            if (cameraHandler != null)
-            {
-                cameraHandler.FollowTarget(delta);
-                cameraHandler.HandleCameraRotation(delta, mouseX, mouseY);
-            }
-        }
 
         public void OnEnable()
         {
