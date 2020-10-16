4cee7ab44f0919850e263ccef5c74c2116eeabd1
Ricardo <ricardolemedossantos@yahoo.com>
2020-10-16 12:15:05 -0300

    Novo

---
+++ Assets/Scripts/WeaponItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DS
+{
+    [CreateAssetMenu(menuName = "Item/Weapon Item")]
+    public class WeaponItem : Item
+    {
+        public GameObject modePrefab;
+        public bool isUnarmed;
+
+        [Header("One Handed Attack Animations")]
+        public string OH_Light_Attack_1;
+        public string OH_Heavy_Attack_1;
+    }
+}
+
