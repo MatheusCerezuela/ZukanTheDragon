dff4f9ddf528bd229ecb741b6bcaa0de21600adb
Ricardo <ricardolemedossantos@yahoo.com>
2020-10-18 07:47:59 -0300

    Atualizado novamente

--- Assets/Scripts/PlayerLocomotion.cs
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -207,7 +207,7 @@ namespace DS
                     }
                     else
                     {
-                        animatorHandler.PlayTargetAnimation("Locomotion", false);
+                        animatorHandler.PlayTargetAnimation("Empty", false);
                         inAirTimer = 0;
                     }
 
@@ -246,6 +246,15 @@ namespace DS
                 {
                     myTransform.position = targetPosition;
                 }
+            }
+
+            if (playerManager.isInteracting || inputHandler.moveAmount > 0)
+            {
+                myTransform.position = Vector3.Lerp(myTransform.position, targetPosition, Time.deltaTime / 0.1f);
+            }
+            else
+            {
+                myTransform.position = targetPosition;
             }
         }
 
